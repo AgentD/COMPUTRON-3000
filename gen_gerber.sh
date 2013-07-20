d8b063bf353fb17b89e5f21b20d20ce653f328e9
David Oberhollenzer <david.oberhollenzer@tele2.at>
2013-07-20 23:52:08 +0200

    Updated README file and gerber file generator

--- gen_gerber.sh
+++ gen_gerber.sh
@@ -4,6 +4,7 @@ mkdir out
 
 pcb -x gerber --metric cpu.pcb --copy-outline all --gerberfile cpu
 pcb -x gerber --metric mem.pcb --copy-outline all --gerberfile mem
+pcb -x gerber --metric uart.pcb --copy-outline all --gerberfile uart
 
 mv *.gbr ./out/
 mv *.cnc ./out/
