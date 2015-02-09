a9e0136c25c1161d3dece806d0d6bf40048ee6f7
David Oberhollenzer <david.oberhollenzer@tele2.at>
2015-02-09 21:53:02 +0100

    Add IDE pcb
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- gen_gerber.sh
+++ gen_gerber.sh
@@ -2,9 +2,11 @@
 
 mkdir out
 
-pcb -x gerber --metric cpu.pcb --copy-outline all --gerberfile cpu
-pcb -x gerber --metric mem.pcb --copy-outline all --gerberfile mem
-pcb -x gerber --metric uart.pcb --copy-outline all --gerberfile uart
+pcb -x gerber cpu.pcb --copy-outline all --gerberfile cpu
+pcb -x gerber mem.pcb --copy-outline all --gerberfile mem
+pcb -x gerber uart.pcb --copy-outline all --gerberfile uart
+pcb -x gerber ide.pcb --copy-outline all --gerberfile ide
+pcb -x gerber himem.pcb --copy-outline all --gerberfile himem
 
 mv *.gbr ./out/
 mv *.cnc ./out/
