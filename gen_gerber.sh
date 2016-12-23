89cd3c68a3697a126629983cfbf01d99ed92b7a8
David Oberhollenzer <david.oberhollenzer@tele2.at>
2016-12-24 00:28:52 +0100

    Update gerber generator script
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- gen_gerber.sh
+++ gen_gerber.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-mkdir out
+mkdir -p out
 
-pcb -x gerber cpu.pcb --copy-outline all --gerberfile cpu
-pcb -x gerber mem.pcb --copy-outline all --gerberfile mem
-pcb -x gerber uart.pcb --copy-outline all --gerberfile uart
-pcb -x gerber ide.pcb --copy-outline all --gerberfile ide
-pcb -x gerber himem.pcb --copy-outline all --gerberfile himem
+for pcb in *.pcb
+do
+	name=$(basename $pcb .pcb)
 
-mv *.gbr ./out/
-mv *.cnc ./out/
+	pcb -x gerber $pcb --copy-outline all --gerberfile $name
 
+	mv $name.*.{gbr,cnc} ./out/
+done
